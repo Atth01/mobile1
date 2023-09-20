5339aa2de4346e59c4c0df15fca4ac3d28c2b886
Atthariq Haykal Putera <atthariqhaykal@gmail.com>
2023-09-20 17:43:20 +0700

    mobile1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import {storage} from '@ionic/-storageangular';
+import { NavController, platform} from '@ionic/angular';
+//import {statusbar} from '@ionic-native/status-bar/ngx';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
+  providers: [storage],
 })
 export class AppComponent {
-  constructor() {}
-}
+  constructor(
+  private storage: Storage,
+
+  private navctrl : NavController,
+  private platfrom : platform
+  //private statusBar : statusBar
+  
+){
+  this.initialiazeApp();
+  }
+
+  async initialiazeApp(){
+     await this.storage.create();
+     this.platfrom.ready().then(() =>{
+      //this.statusBar.styleDefault();
+     });
+     this.storage.get('isLoggedIn').then (val)=> {
+      if (val=== null || val === undefined || vall === ''){
+        this.navctrl.navigateRoot('/login');
+
+      }else {
+        this.navctrl.navigateRoot('/tabs/tabs');
+      }
+     }
+  }
+    
+  }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
+import{ IonicStorageModule} from  '@ionic/storage-angular';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-  bootstrap: [AppComponent],
+  import : [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    IonicStorageModule.forRoot()
+
+  ],
+  providers: [
+    //statusBar,
+    { provide: RouteReuseStrategy,useClass: IonicRouteStrategy},
+
+  ],
+  bootstrap:[AppComponent],
 })
-export class AppModule {}
+export class AppModule{}
+  
